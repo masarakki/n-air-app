1bd9d028ab360ac64c8396d01387aefd9f75f0fa
eddyStreamlabs <eddy.gharbi@streamlabs.com>
2018-08-01 13:32:44 -0700

    Only test adding and removing source

--- test/sources.js
+++ test/sources.js
@@ -45,33 +45,524 @@ test('Adding and removing some sources', async t => {
   }
 });
 
-test('Viewing source properties', async t => {
+test('Adding and removing some sources', async t => {
   const app = t.context.app;
-  const sourceName = 'Cool Color Source';
 
-  await addSource(t, 'Color Source', sourceName);
+  for (const sourceType of sourceTypes) {
+    const sourceName = `Example ${sourceType}`;
 
-  await focusMain(t);
-  await selectSource(t, sourceName);
-  await clickSourceProperties(t);
+    await addSource(t, sourceType, sourceName);
+    await focusMain(t);
 
-  await focusChild(t);
-  t.true(await app.client.isExisting('label=Color'));
+    t.true(await sourceIsExisting(t, sourceName));
+
+    await selectSource(t, sourceName);
+    await clickRemoveSource(t);
+
+    t.false(await sourceIsExisting(t, sourceName));
+  }
 });
 
-
-test('Rename source', async t => {
+test('Adding and removing some sources', async t => {
   const app = t.context.app;
-  const sourceName = 'MyColorSource1';
-  const newSourceName = 'MyColorSource2';
 
-  await addSource(t, 'Color Source', sourceName);
+  for (const sourceType of sourceTypes) {
+    const sourceName = `Example ${sourceType}`;
 
-  await openRenameWindow(t, sourceName);
-  await app.client.setValue('input', newSourceName);
-  await app.client.click('button=Done');
+    await addSource(t, sourceType, sourceName);
+    await focusMain(t);
 
+    t.true(await sourceIsExisting(t, sourceName));
 
-  await focusMain(t);
-  t.true(await sourceIsExisting(t, newSourceName));
+    await selectSource(t, sourceName);
+    await clickRemoveSource(t);
+
+    t.false(await sourceIsExisting(t, sourceName));
+  }
+});
+
+test('Adding and removing some sources', async t => {
+  const app = t.context.app;
+
+  for (const sourceType of sourceTypes) {
+    const sourceName = `Example ${sourceType}`;
+
+    await addSource(t, sourceType, sourceName);
+    await focusMain(t);
+
+    t.true(await sourceIsExisting(t, sourceName));
+
+    await selectSource(t, sourceName);
+    await clickRemoveSource(t);
+
+    t.false(await sourceIsExisting(t, sourceName));
+  }
+});
+
+test('Adding and removing some sources', async t => {
+  const app = t.context.app;
+
+  for (const sourceType of sourceTypes) {
+    const sourceName = `Example ${sourceType}`;
+
+    await addSource(t, sourceType, sourceName);
+    await focusMain(t);
+
+    t.true(await sourceIsExisting(t, sourceName));
+
+    await selectSource(t, sourceName);
+    await clickRemoveSource(t);
+
+    t.false(await sourceIsExisting(t, sourceName));
+  }
+});
+
+test('Adding and removing some sources', async t => {
+  const app = t.context.app;
+
+  for (const sourceType of sourceTypes) {
+    const sourceName = `Example ${sourceType}`;
+
+    await addSource(t, sourceType, sourceName);
+    await focusMain(t);
+
+    t.true(await sourceIsExisting(t, sourceName));
+
+    await selectSource(t, sourceName);
+    await clickRemoveSource(t);
+
+    t.false(await sourceIsExisting(t, sourceName));
+  }
+});
+
+test('Adding and removing some sources', async t => {
+  const app = t.context.app;
+
+  for (const sourceType of sourceTypes) {
+    const sourceName = `Example ${sourceType}`;
+
+    await addSource(t, sourceType, sourceName);
+    await focusMain(t);
+
+    t.true(await sourceIsExisting(t, sourceName));
+
+    await selectSource(t, sourceName);
+    await clickRemoveSource(t);
+
+    t.false(await sourceIsExisting(t, sourceName));
+  }
+});
+
+test('Adding and removing some sources', async t => {
+  const app = t.context.app;
+
+  for (const sourceType of sourceTypes) {
+    const sourceName = `Example ${sourceType}`;
+
+    await addSource(t, sourceType, sourceName);
+    await focusMain(t);
+
+    t.true(await sourceIsExisting(t, sourceName));
+
+    await selectSource(t, sourceName);
+    await clickRemoveSource(t);
+
+    t.false(await sourceIsExisting(t, sourceName));
+  }
+});
+
+test('Adding and removing some sources', async t => {
+  const app = t.context.app;
+
+  for (const sourceType of sourceTypes) {
+    const sourceName = `Example ${sourceType}`;
+
+    await addSource(t, sourceType, sourceName);
+    await focusMain(t);
+
+    t.true(await sourceIsExisting(t, sourceName));
+
+    await selectSource(t, sourceName);
+    await clickRemoveSource(t);
+
+    t.false(await sourceIsExisting(t, sourceName));
+  }
+});
+
+test('Adding and removing some sources', async t => {
+  const app = t.context.app;
+
+  for (const sourceType of sourceTypes) {
+    const sourceName = `Example ${sourceType}`;
+
+    await addSource(t, sourceType, sourceName);
+    await focusMain(t);
+
+    t.true(await sourceIsExisting(t, sourceName));
+
+    await selectSource(t, sourceName);
+    await clickRemoveSource(t);
+
+    t.false(await sourceIsExisting(t, sourceName));
+  }
+});
+
+test('Adding and removing some sources', async t => {
+  const app = t.context.app;
+
+  for (const sourceType of sourceTypes) {
+    const sourceName = `Example ${sourceType}`;
+
+    await addSource(t, sourceType, sourceName);
+    await focusMain(t);
+
+    t.true(await sourceIsExisting(t, sourceName));
+
+    await selectSource(t, sourceName);
+    await clickRemoveSource(t);
+
+    t.false(await sourceIsExisting(t, sourceName));
+  }
+});
+
+test('Adding and removing some sources', async t => {
+  const app = t.context.app;
+
+  for (const sourceType of sourceTypes) {
+    const sourceName = `Example ${sourceType}`;
+
+    await addSource(t, sourceType, sourceName);
+    await focusMain(t);
+
+    t.true(await sourceIsExisting(t, sourceName));
+
+    await selectSource(t, sourceName);
+    await clickRemoveSource(t);
+
+    t.false(await sourceIsExisting(t, sourceName));
+  }
+});
+
+test('Adding and removing some sources', async t => {
+  const app = t.context.app;
+
+  for (const sourceType of sourceTypes) {
+    const sourceName = `Example ${sourceType}`;
+
+    await addSource(t, sourceType, sourceName);
+    await focusMain(t);
+
+    t.true(await sourceIsExisting(t, sourceName));
+
+    await selectSource(t, sourceName);
+    await clickRemoveSource(t);
+
+    t.false(await sourceIsExisting(t, sourceName));
+  }
+});
+
+test('Adding and removing some sources', async t => {
+  const app = t.context.app;
+
+  for (const sourceType of sourceTypes) {
+    const sourceName = `Example ${sourceType}`;
+
+    await addSource(t, sourceType, sourceName);
+    await focusMain(t);
+
+    t.true(await sourceIsExisting(t, sourceName));
+
+    await selectSource(t, sourceName);
+    await clickRemoveSource(t);
+
+    t.false(await sourceIsExisting(t, sourceName));
+  }
+});
+
+test('Adding and removing some sources', async t => {
+  const app = t.context.app;
+
+  for (const sourceType of sourceTypes) {
+    const sourceName = `Example ${sourceType}`;
+
+    await addSource(t, sourceType, sourceName);
+    await focusMain(t);
+
+    t.true(await sourceIsExisting(t, sourceName));
+
+    await selectSource(t, sourceName);
+    await clickRemoveSource(t);
+
+    t.false(await sourceIsExisting(t, sourceName));
+  }
+});
+
+test('Adding and removing some sources', async t => {
+  const app = t.context.app;
+
+  for (const sourceType of sourceTypes) {
+    const sourceName = `Example ${sourceType}`;
+
+    await addSource(t, sourceType, sourceName);
+    await focusMain(t);
+
+    t.true(await sourceIsExisting(t, sourceName));
+
+    await selectSource(t, sourceName);
+    await clickRemoveSource(t);
+
+    t.false(await sourceIsExisting(t, sourceName));
+  }
+});
+
+test('Adding and removing some sources', async t => {
+  const app = t.context.app;
+
+  for (const sourceType of sourceTypes) {
+    const sourceName = `Example ${sourceType}`;
+
+    await addSource(t, sourceType, sourceName);
+    await focusMain(t);
+
+    t.true(await sourceIsExisting(t, sourceName));
+
+    await selectSource(t, sourceName);
+    await clickRemoveSource(t);
+
+    t.false(await sourceIsExisting(t, sourceName));
+  }
+});
+
+test('Adding and removing some sources', async t => {
+  const app = t.context.app;
+
+  for (const sourceType of sourceTypes) {
+    const sourceName = `Example ${sourceType}`;
+
+    await addSource(t, sourceType, sourceName);
+    await focusMain(t);
+
+    t.true(await sourceIsExisting(t, sourceName));
+
+    await selectSource(t, sourceName);
+    await clickRemoveSource(t);
+
+    t.false(await sourceIsExisting(t, sourceName));
+  }
+});
+
+test('Adding and removing some sources', async t => {
+  const app = t.context.app;
+
+  for (const sourceType of sourceTypes) {
+    const sourceName = `Example ${sourceType}`;
+
+    await addSource(t, sourceType, sourceName);
+    await focusMain(t);
+
+    t.true(await sourceIsExisting(t, sourceName));
+
+    await selectSource(t, sourceName);
+    await clickRemoveSource(t);
+
+    t.false(await sourceIsExisting(t, sourceName));
+  }
+});
+
+test('Adding and removing some sources', async t => {
+  const app = t.context.app;
+
+  for (const sourceType of sourceTypes) {
+    const sourceName = `Example ${sourceType}`;
+
+    await addSource(t, sourceType, sourceName);
+    await focusMain(t);
+
+    t.true(await sourceIsExisting(t, sourceName));
+
+    await selectSource(t, sourceName);
+    await clickRemoveSource(t);
+
+    t.false(await sourceIsExisting(t, sourceName));
+  }
+});
+
+test('Adding and removing some sources', async t => {
+  const app = t.context.app;
+
+  for (const sourceType of sourceTypes) {
+    const sourceName = `Example ${sourceType}`;
+
+    await addSource(t, sourceType, sourceName);
+    await focusMain(t);
+
+    t.true(await sourceIsExisting(t, sourceName));
+
+    await selectSource(t, sourceName);
+    await clickRemoveSource(t);
+
+    t.false(await sourceIsExisting(t, sourceName));
+  }
+});
+
+test('Adding and removing some sources', async t => {
+  const app = t.context.app;
+
+  for (const sourceType of sourceTypes) {
+    const sourceName = `Example ${sourceType}`;
+
+    await addSource(t, sourceType, sourceName);
+    await focusMain(t);
+
+    t.true(await sourceIsExisting(t, sourceName));
+
+    await selectSource(t, sourceName);
+    await clickRemoveSource(t);
+
+    t.false(await sourceIsExisting(t, sourceName));
+  }
+});
+
+test('Adding and removing some sources', async t => {
+  const app = t.context.app;
+
+  for (const sourceType of sourceTypes) {
+    const sourceName = `Example ${sourceType}`;
+
+    await addSource(t, sourceType, sourceName);
+    await focusMain(t);
+
+    t.true(await sourceIsExisting(t, sourceName));
+
+    await selectSource(t, sourceName);
+    await clickRemoveSource(t);
+
+    t.false(await sourceIsExisting(t, sourceName));
+  }
+});
+
+test('Adding and removing some sources', async t => {
+  const app = t.context.app;
+
+  for (const sourceType of sourceTypes) {
+    const sourceName = `Example ${sourceType}`;
+
+    await addSource(t, sourceType, sourceName);
+    await focusMain(t);
+
+    t.true(await sourceIsExisting(t, sourceName));
+
+    await selectSource(t, sourceName);
+    await clickRemoveSource(t);
+
+    t.false(await sourceIsExisting(t, sourceName));
+  }
+});
+
+test('Adding and removing some sources', async t => {
+  const app = t.context.app;
+
+  for (const sourceType of sourceTypes) {
+    const sourceName = `Example ${sourceType}`;
+
+    await addSource(t, sourceType, sourceName);
+    await focusMain(t);
+
+    t.true(await sourceIsExisting(t, sourceName));
+
+    await selectSource(t, sourceName);
+    await clickRemoveSource(t);
+
+    t.false(await sourceIsExisting(t, sourceName));
+  }
+});
+
+test('Adding and removing some sources', async t => {
+  const app = t.context.app;
+
+  for (const sourceType of sourceTypes) {
+    const sourceName = `Example ${sourceType}`;
+
+    await addSource(t, sourceType, sourceName);
+    await focusMain(t);
+
+    t.true(await sourceIsExisting(t, sourceName));
+
+    await selectSource(t, sourceName);
+    await clickRemoveSource(t);
+
+    t.false(await sourceIsExisting(t, sourceName));
+  }
+});
+
+test('Adding and removing some sources', async t => {
+  const app = t.context.app;
+
+  for (const sourceType of sourceTypes) {
+    const sourceName = `Example ${sourceType}`;
+
+    await addSource(t, sourceType, sourceName);
+    await focusMain(t);
+
+    t.true(await sourceIsExisting(t, sourceName));
+
+    await selectSource(t, sourceName);
+    await clickRemoveSource(t);
+
+    t.false(await sourceIsExisting(t, sourceName));
+  }
+});
+
+test('Adding and removing some sources', async t => {
+  const app = t.context.app;
+
+  for (const sourceType of sourceTypes) {
+    const sourceName = `Example ${sourceType}`;
+
+    await addSource(t, sourceType, sourceName);
+    await focusMain(t);
+
+    t.true(await sourceIsExisting(t, sourceName));
+
+    await selectSource(t, sourceName);
+    await clickRemoveSource(t);
+
+    t.false(await sourceIsExisting(t, sourceName));
+  }
+});
+
+test('Adding and removing some sources', async t => {
+  const app = t.context.app;
+
+  for (const sourceType of sourceTypes) {
+    const sourceName = `Example ${sourceType}`;
+
+    await addSource(t, sourceType, sourceName);
+    await focusMain(t);
+
+    t.true(await sourceIsExisting(t, sourceName));
+
+    await selectSource(t, sourceName);
+    await clickRemoveSource(t);
+
+    t.false(await sourceIsExisting(t, sourceName));
+  }
+});
+
+test('Adding and removing some sources', async t => {
+  const app = t.context.app;
+
+  for (const sourceType of sourceTypes) {
+    const sourceName = `Example ${sourceType}`;
+
+    await addSource(t, sourceType, sourceName);
+    await focusMain(t);
+
+    t.true(await sourceIsExisting(t, sourceName));
+
+    await selectSource(t, sourceName);
+    await clickRemoveSource(t);
+
+    t.false(await sourceIsExisting(t, sourceName));
+  }
 });
